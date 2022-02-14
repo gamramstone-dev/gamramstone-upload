d6f928c800754234386c7b31345323a069474ec6
So-chiru <unleash9224@gmail.com>
2022-02-14 16:09:41 +0900

    Init auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,26 +3,36 @@ import { useRouter } from 'next/router'
 import styles from '../styles/components/Header.module.scss'
 import { Button } from './Button'
 
+import { useSession, signIn, signOut } from 'next-auth/react'
+
 export const Header = () => {
   const router = useRouter()
+  const { data: session } = useSession()
+
+  console.log(session)
 
   return (
     <header className={styles.header}>
       <div className={styles.contents}>
         <div className={styles.logo}>
-          <Link href={'/'}>
-            GAMRAMSTONE
-          </Link>
+          <Link href={'/'}>GAMRAMSTONE</Link>
         </div>
         <div className={styles.actions}>
-          <Button
-            size='medium'
-            roundness={16}
-            onClick={() => router.push('/api/login')}
-            disabled={true}
-          >
-            로그인
-          </Button>
+          {session ? (
+            session.user?.name
+          ) : (
+            <Button
+              size='medium'
+              roundness={16}
+              onClick={() =>
+                signIn('google', {
+                  scope: 'auth/youtube.force-ssl',
+                })
+              }
+            >
+              로그인
+            </Button>
+          )}
         </div>
       </div>
     </header>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { RecoilRoot } from 'recoil'
 import { motion } from 'framer-motion'
 import Header from '../components/Header'
 import { Toaster } from 'react-hot-toast'
+import { SessionProvider } from 'next-auth/react'
 
 const variants = {
   hidden: { opacity: 0 },
@@ -14,23 +15,29 @@ const variants = {
   exit: { opacity: 0 },
 }
 
-function MyApp ({ Component, pageProps, router }: AppProps) {
+function MyApp ({
+  Component,
+  pageProps: { session, ...pageProps },
+  router,
+}: AppProps) {
   return (
-    <RecoilRoot>
-      <Header></Header>
-      <Toaster position='top-center'></Toaster>
-      <motion.div
-        key={router.route}
-        variants={variants}
-        className={'page-wrapper'}
-        initial='hidden'
-        animate='enter'
-        exit='exit'
-        transition={{ type: 'spring', stiffness: 200, damping: 20 }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </RecoilRoot>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Header></Header>
+        <Toaster position='top-center'></Toaster>
+        <motion.div
+          key={router.route}
+          variants={variants}
+          className={'page-wrapper'}
+          initial='hidden'
+          animate='enter'
+          exit='exit'
+          transition={{ type: 'spring', stiffness: 200, damping: 20 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </RecoilRoot>
+    </SessionProvider>
   )
 }
 
