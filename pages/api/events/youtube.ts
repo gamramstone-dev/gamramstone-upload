b2f03449bf112a9448c1ad4d47afa3a6d96aca3e
So-chiru <unleash9224@gmail.com>
2022-03-14 21:07:19 +0900

    Update youtube video hook handler to handle...
    multiple video update requests.

--- pages/api/events/youtube.ts
+++ pages/api/events/youtube.ts
@@ -1,5 +1,6 @@
 import Airtable from 'airtable'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { getFirstItem } from '../../../structs/airtable'
 import { apify } from '../../../structs/api'
 import { ChannelID, getChannelByID } from '../../../structs/channels'
 
@@ -9,6 +10,16 @@ import { getYouTubeVideoSnippetDetails } from '../../../utils/server/youtube'
 const base = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY,
 }).base(process.env.AIRTABLE_BASE_ID!)
+
+const cutMessage = (str: unknown, maxLength: number): string | boolean => {
+  if (typeof str !== 'string') {
+    return false
+  }
+
+  return str.length - 3 > maxLength
+    ? str.substring(0, maxLength - 3) + '...'
+    : str
+}
 
 const ChannelMessages: Record<ChannelID, string[]> = {
   gosegu: ['$t 영상 업로드 킹아!!! ^ㅁ^', '$t 영상이 올라왔습니다!'],
@@ -95,35 +106,139 @@ const func = async (req: NextApiRequest, res: NextApiResponse) => {
     const { title, description, publishedAt } = youtubeSnippet
     const duration = parseDurationString(contentDetails.duration) || 0
 
-    discord.sendSimpleMessage(
-      process.env.DISCORD_YOUTUBE_HOOK!,
-      `${ChannelMessages[channelObject.id as ChannelID][
-        Math.floor(
-          Math.random() * ChannelMessages[channelObject.id as ChannelID].length
-        )
-      ].replace(/\$t/g, title)} https://youtube.com/watch?v=${id}`,
-      {
-        username: channelObject.name,
-        avatar_url: channelObject.image,
-      }
-    )
-
     const table = base('영상 정보 (자동 업데이트)')
 
     console.log(`[youtube] ${title} uploading...`)
 
-    await table.create([
-      {
-        fields: {
+    const field = await table
+      .select({
+        filterByFormula: `{URL} = "https://www.youtube.com/watch?v=${id}"`,
+      })
+      .all()
+
+    console.log(`[youtube] ${title} - ${field.length} records exist.`)
+
+    /**
+     * 필드가 하나 있는 부분) 제목, 설명이 다를 경우 업데이트
+     */
+    if (field.length === 1) {
+      const recordTitle = getFirstItem(field[0].get('제목'))
+      const recordDescription = getFirstItem(field[0].get('세부 정보'))
+
+      let update = {
+        title: false,
+        description: false,
+      }
+
+      if (recordTitle !== title) {
+        await table.update(field[0].id, {
           제목: title,
-          채널: channelObject.name,
+        })
+
+        update.title = true
+      }
+
+      if (recordDescription !== description) {
+        await table.update(field[0].id, {
           '세부 정보': description,
-          URL: `https://www.youtube.com/watch?v=${id}`,
-          '업로드 날짜': publishedAt,
-          '영상 길이': duration,
+        })
+
+        update.description = true
+      }
+
+      if (update.title || update.description) {
+        discord.sendEmbedMessage(
+          process.env.DISCORD_YOUTUBE_HOOK!,
+          [
+            {
+              color: 0x00ff00,
+              title: recordTitle,
+              description: `YouTube 영상 ${Object.entries(update)
+                .filter(v => v[1])
+                .map(v => ({ title: '제목', description: '세부 정보' }[v[0]]))
+                .join(', ')} 데이터가 업데이트되어 레코드를 갱신했습니다.`,
+              url: `https://www.youtube.com/watch?v=${id}`,
+              fields: [
+                ...(update.title
+                  ? [
+                      {
+                        name: '변경 전 제목',
+                        value: (cutMessage(recordTitle, 1020) || '-') as string,
+                      },
+                      {
+                        name: '변경 후 제목',
+                        value: (cutMessage(title, 1020) || '-') as string,
+                      },
+                    ]
+                  : []),
+                ...(update.description
+                  ? [
+                      {
+                        name: '변경 전 세부 정보',
+                        value: (cutMessage(recordDescription, 1020) ||
+                          '-') as string,
+                      },
+                      {
+                        name: '변경 후 세부 정보',
+                        value: (cutMessage(description, 1020) || '-') as string,
+                      },
+                    ]
+                  : []),
+              ],
+            },
+          ],
+          {
+            username: channelObject.name,
+            avatar_url: channelObject.image,
+          }
+        )
+      }
+    } else if (field.length > 1) {
+      discord.sendEmbedMessage(
+        process.env.DISCORD_YOUTUBE_HOOK!,
+        [
+          {
+            color: 0xff0000,
+            title: '영상 정보 충돌',
+            description: `같은 URL을 가진 영상이 여러개 있습니다. 중복 레코드를 삭제해주세요: ${field
+              .map(v => v.id)
+              .join(', ')}`,
+            url: `https://www.youtube.com/watch?v=${id}`,
+          },
+        ],
+        {
+          username: channelObject.name,
+          avatar_url: channelObject.image,
+        }
+      )
+    } else {
+      await table.create([
+        {
+          fields: {
+            제목: title,
+            채널: channelObject.name,
+            '세부 정보': description,
+            URL: `https://www.youtube.com/watch?v=${id}`,
+            '업로드 날짜': publishedAt,
+            '영상 길이': duration,
+          },
         },
-      },
-    ])
+      ])
+
+      discord.sendSimpleMessage(
+        process.env.DISCORD_YOUTUBE_HOOK!,
+        `${ChannelMessages[channelObject.id as ChannelID][
+          Math.floor(
+            Math.random() *
+              ChannelMessages[channelObject.id as ChannelID].length
+          )
+        ].replace(/\$t/g, title)} https://youtube.com/watch?v=${id}`,
+        {
+          username: channelObject.name,
+          avatar_url: channelObject.image,
+        }
+      )
+    }
 
     return
   } catch (e) {
