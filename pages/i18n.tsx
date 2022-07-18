3f16b8cdb80f7c281e494df2e5c82ef2f0e4bfe4
So-chiru <unleash9224@gmail.com>
2022-07-18 10:03:48 +0900

    Upgrade dependencies

--- pages/i18n.tsx
+++ pages/i18n.tsx
@@ -593,7 +593,6 @@ i18n
   .init({
     resources,
     fallbackLng: 'ko',
-    debug: true,
     // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // // if you're using a language detector, do not define the lng option
